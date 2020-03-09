7ead11002b594541fd238748d93936d99c8861e4
Valery Babaev <slmail2009@gmail.com>
2020-03-09 23:42:27 +0700

    Updates

---
+++ precache-manifest.30ed5e65bf93bfc10929f3433858a98e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4654ca48c60456713cf136a24f7901",
+    "url": "/vk-app/index.html"
+  },
+  {
+    "revision": "17ff72d4f6815165a197",
+    "url": "/vk-app/static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "84c668213de7e4fdbe72",
+    "url": "/vk-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "17ff72d4f6815165a197",
+    "url": "/vk-app/static/js/2.35956a2b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/vk-app/static/js/2.35956a2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84c668213de7e4fdbe72",
+    "url": "/vk-app/static/js/main.83f01b37.chunk.js"
+  },
+  {
+    "revision": "98219845035418794c8b",
+    "url": "/vk-app/static/js/runtime-main.dd94d9da.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-app/static/media/persik.4e1ec840.png"
+  }
+]);
